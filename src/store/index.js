339c0ef4bd8dea723afb29e12a4cfc829b539b51
CikSimon <1simoncik1@gmail.com>
2023-06-29 16:18:34 +0200

    learning vuex, basic random counter

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,30 @@
+import axios from 'axios'
 import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-  },
-  getters: {
+    counter: 0
   },
   mutations: {
+    increaseCounter(state, randomNumber){
+      state.counter += randomNumber
+    },
+    decreaseCounter(state, randomNumber){
+      state.counter -= randomNumber
+    }
   },
   actions: {
+    increaseCounter({ commit }){
+      console.log('increaseCounter (action)')
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response => {commit('increaseCounter', response.data)})
+    },
+    decreaseCounter({ commit }){
+      console.log('increaseCounter (action)')
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response => {commit('decreaseCounter', response.data)})
+    }
+  },
+  getters: {
+
   },
   modules: {
   }
